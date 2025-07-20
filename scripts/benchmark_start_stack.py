88b71d15b1065d886064d09f212b3e7d76e30cba
Johannes Nussbaum <39048939+jnussbaum@users.noreply.github.com>
2025-07-20 17:25:38 +0200

    set up benchmark (#2)

---
+++ scripts/benchmark_start_stack.py
@@ -0,0 +1,68 @@
+import os
+import platform
+import subprocess
+import warnings
+from typing import Literal
+
+import pyperf
+
+# ruff: noqa:S607 (Starting a process with a partial executable path)
+
+
+def clear_mac_system_caches() -> None:
+    """Sync filesystem with 'sync' and clear system caches using 'purge'"""
+    subprocess.run(["sync"], check=False)
+    subprocess.run(["purge"], check=False)
+
+
+def clear_linux_system_caches() -> None:
+    """Sync filesystem with 'sync' and clear system caches using 'echo 3 > /proc/sys/vm/drop_caches'"""
+    subprocess.run(["sync"], check=False)
+    subprocess.run(["sudo", "sh", "-c", "sync && echo 3 > /proc/sys/vm/drop_caches"], check=False)
+
+
+def setup() -> None:
+    if platform.system() == "Darwin":
+        pass
+        # clear_mac_system_caches() only works for docker
+    elif platform.system() == "Linux":
+        clear_linux_system_caches()
+    else:
+        warnings.warn(f"{platform.system()=}")
+    subprocess.run(["dsp-tools", "start-stack", "--no-prune"], check=True)
+
+
+def teardown() -> None:
+    subprocess.run(["dsp-tools", "stop-stack"], check=True)
+
+
+def task_to_measure():
+    subprocess.run(["dsp-tools", "start-stack", "--no-prune"], check=True)
+
+
+def main():
+    container_engine: Literal["podman", "docker"] = "docker"
+    os.environ["CONTAINER_ENGINE"] = container_engine
+
+    runner = pyperf.Runner(
+        values=3,  # default = 3
+        warmups=1,  # default = 1
+        processes=4,  # default = 20
+        loops=0,  # default = 0
+        min_time=0.1,  # default = 0.1
+        metadata=None,  # default = None
+        show_name=True,  # default = True
+        program_args=None,  # default = None
+        add_cmdline_args=None,  # default = None
+    )
+    runner.timeit(
+        name="start-stack",
+        stmt="task_to_measure()",
+        setup="setup()",
+        teardown="teardown()",
+        globals=globals() | locals(),
+    )
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/benchmarking_results.md
@@ -0,0 +1,19 @@
+# Benchmarking Results
+
+## Mac
+
+### Modalities
+
+pyperf.Runner which spawns 4 processes, which each run 1 warmup and 3 measurements.
+This results in 12 measurements.
+Before doing the actual measurements, pyperf spawns 1 calibrating process which also runs 1 warmup + 3 measurements.
+So in total, the code is executed 20 times, but only 12 are used for the measurement.
+
+podman
+run start-stack: Mean +- std dev: 15.2 sec +- 0.3 sec
+
+docker
+run start-stack: Mean +- std dev: 12.2 sec +- 0.5 sec
+
+docker (with cache resetting)
+run start-stack: Mean +- std dev: 12.8 sec +- 0.5 sec

562602cd2ce060ed502604f84c91db146e7eece1
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2025-07-25 22:16:23 +0200

    edit

--- scripts/benchmark_xmlupload_docker.py
+++ scripts/benchmark_xmlupload_docker.py
@@ -1,7 +1,5 @@
 import os
-import platform
 import subprocess
-import warnings
 
 import pyperf
 from loguru import logger
@@ -9,29 +7,7 @@ from loguru import logger
 # ruff: noqa:S607 (Starting a process with a partial executable path)
 
 
-def clear_mac_system_caches() -> None:
-    """Sync filesystem with 'sync' and clear system caches using 'purge'"""
-    logger.info("Clearing Mac system caches")
-    subprocess.run(["sync"], check=False)
-    subprocess.run(["purge"], check=False)
-
-
-def clear_linux_system_caches() -> None:
-    """Sync filesystem with 'sync' and clear system caches using 'echo 3 > /proc/sys/vm/drop_caches'"""
-    logger.info("Clearing Linux system caches")
-    subprocess.run(["sync"], check=False)
-    subprocess.run(["sudo", "sh", "-c", "sync && echo 3 > /proc/sys/vm/drop_caches"], check=False)
-
-
 def setup() -> None:
-    logger.info("Entering setup")
-    if platform.system() == "Darwin":
-        pass
-        # clear_mac_system_caches() only works for docker
-    elif platform.system() == "Linux":
-        clear_linux_system_caches()
-    else:
-        warnings.warn(f"{platform.system()=}")
     logger.info("Run 'dsp-tools start-stack'...")
     subprocess.run(["dsp-tools", "start-stack", "--no-prune"], check=True)
 
@@ -54,7 +30,7 @@ def main():
     runner = pyperf.Runner(
         values=3,  # default = 3
         warmups=1,  # default = 1
-        processes=4,  # default = 20
+        processes=10,  # default = 20
         loops=0,  # default = 0
         min_time=0.1,  # default = 0.1
         metadata=None,  # default = None
